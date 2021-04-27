1bf3ed5c05d3940bd0025e2c948f8712e1fb43a3
kevinelstri <1098918523@qq.com>
2021-04-27 22:35:31 +0800

    Add files via upload

---
+++ Linear Regression/LR.py
@@ -0,0 +1,10 @@
+import numpy as np
+import math
+
+'''
+Author:kevinelstri
+Date:2021/04/27
+Desc:线性回归
+Source:线性代数知识汇总 https://blog.csdn.net/myarrow/article/details/53365048
+       Numpy矩阵运算 
+'''
